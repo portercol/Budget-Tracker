a97a1f69b815dc46bc604133220dd37b80bec894
Collin Porter <colls.potta@gmail.com>
2020-12-01 10:47:20 -0700

    creating function for getting all records from the db, creating a post route, then clearning the db after the post. Added event listener for when the app is online and also added this function to the onsuccess request above

--- public/db.js
+++ public/db.js
@@ -4,28 +4,67 @@ let db;
 // Create a new request for indexedDB
 const request = indexedDB.open('budgetTracker', 1);
 
-// After onupgradeneeded, open an objectStore
+// Upon onupgradeneeded, create objectStore
 request.onupgradeneeded = function (event) {
   db = event.target.result;
   db.createObjectStore('transaction', { autoIncrement: true });
 };
 
-// Upon onsuccess, run a potential function for checking the database
+// Upon onsuccess/online, run checkDB function
 request.onsuccess = function (event) {
   db = event.target.result;
+  if (window.onLine) {
+    checkDB();
+  }
 };
 
-// When/IF error, log the error
+// Upon onerror, log the error
 request.onerror = function (event) {
   console.log(event.target.error);
 };
 
-// Create function for saving records if/when transaction is made and/or fails
+// Create function for saving records and adding them to the object store
 function saveRecord(record) {
-  // create transaction on the objectStore with readwrite access
+  // create a transaction on the objectStore with readwrite access
   const transaction = db.transaction('transaction', 'readwrite');
-  // getting access to the objectStore
+  // access the objectStore
   const store = transaction.objectStore('transaction');
-  // adding the record to the store
+  // add record to the store
   store.add(record);
 };
+
+// Create function for getting all records, posting them and then clearning them if needed
+function checkDB() {
+  // create a transaction on the objectStore with readwrite access
+  const transaction = db.transaction('transaction', 'readwrite');
+  // access the objectStore
+  const store = transaction.objectStore('transaction');
+  // get all records from the store
+  const getAll = store.getAll();
+
+  // If successful, then POST the records
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then((response) => response.json())
+        .then(() => {
+          //clear indexeddb store after successful POST
+          const transaction = db.transaction(['transaction'], 'readwrite');
+          // access object store
+          const store = transaction.objectStore('transaction');
+          // clear all items in object store
+          store.clear();
+        });
+    }
+  };
+}
+
+// Listen for app when back online
+window.addEventListener('online', checkDB);
