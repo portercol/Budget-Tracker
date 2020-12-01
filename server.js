777991952c1fb6825234d224210b3fda05ad2e6e
Collin Porter <colls.potta@gmail.com>
2020-12-01 11:19:36 -0700

    added comments and brought in environment variable through dotenv package

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
+// Require npm packages
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// Require dotenv package to use environment variables
+require('dotenv').congig();
 
+// Create PORT to run on whatever is available OR PORT 3000
+const PORT = process.env.PORT || 3000;
+
+// Create express server
 const app = express();
 
+// Use logger middleware
 app.use(logger("dev"));
 
+// Parse request body as JSON
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve static content for the app
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
@@ -20,9 +29,10 @@ mongoose.connect("mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
-// routes
+// Use routes being brought in
 app.use(require("./routes/api.js"));
 
+// Create server listener on PORT
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
